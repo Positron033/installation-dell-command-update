4de9085f0931b44a6170ac52348d997c827270c8
Positron033 <60300994+Positron033@users.noreply.github.com>
2021-01-24 17:01:43 +0100

    mise forme

--- installation de dell-command-update.ps1
+++ installation de dell-command-update.ps1
@@ -5,7 +5,8 @@ New-Item -Path "c:\temp -ItemType Directory"
 Invoke-WebRequest -Uri "https://dl.dell.com/FOLDER06231658M/1/Dell-Command-Update-Application-for-Windows-10_34T96_WIN_3.1.2_A00.EXE"  -OutFile "C:\temp\Dell-Command-Update-Application-for-Windows-10_34T96_WIN_3.1.2_A00.EXE" 
 
 #verification du téléchargement du fichier
-if (Test-Path -path "C:\temp\Dell-Command-Update-Application-for-Windows-10_34T96_WIN_3.1.2_A00.EXE") {
+if (Test-Path -path "C:\temp\Dell-Command-Update-Application-for-Windows-10_34T96_WIN_3.1.2_A00.EXE") 
+{
     #lancement de l'installation
     write-host "installation de dell command update en cours..." -ForegroundColor Green
     Start-Process C:\temp\Dell-Command-Update-Application-for-Windows-10_34T96_WIN_3.1.2_A00.EXE -PassThru -ArgumentList "/S" -wait 
